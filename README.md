e9098524b88eddd929ff5238b67d8ff8d07556cc
Eddy Hood <eddy@ignitespot.com>
2017-07-23 00:51:28 -0600

    added model tests

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>Project 5: Learning Journal</h1>
+
+This application is an online learing journal.  Users can perform
+the following tasks:
+
+* Register to create an account
+* Log in and log out
+* Add journal entires under their accounts
+* Edit journal entries
+
+<h3>This project focuses on the following skills:</h3>
+* Python programming
+* PEEWEE ORM & SQLite3 database management
+* Flask Applications & extensions
+* Basic security features like password hashing
+
+
--- tests.py
+++ tests.py
@@ -1,2 +1,56 @@
 import unittest
 
+import app
+from models import User, Journal, Tag
+
+
+class ModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.fake_user = User.create_user(
+                                username='fake_$_user',
+                                email='fake@fakeemail.com',
+                                password='password'
+                                )
+        self.fake_journal = Journal.create(
+                                user=1,
+                                title='fake journal entry',
+                                date=07/29/1980,
+                                time=100,
+                                entry="I have learned a lot.",
+                                resources="Take more Treehouse courses")
+        self.fake_tag = Tag.create(
+                            journal=1,
+                            tag='fake_tag')
+
+    def test_create_user(self):
+        """See of a user was created in the setup """
+        self.assertTrue(User.get(User.username == 'fake_$_user'))
+
+    def test_create_duplicate_user(self):
+        """Try to create a duplicate user and see if ValueError is raised """
+        with self.assertRaises(ValueError):
+            User.create_user(
+                    username='fake_$_user',
+                    email='fake@fakeemail.com',
+                    password='password'
+                    )
+
+    def test_password_hash(self):
+        """See if the password was hashed or if it stayed 'password' """
+        self.assertNotEqual(User.select().get().password, 'password')
+
+    def test_create_journal(self):
+        """See of a journal was created in the setup """
+        self.assertTrue(Journal.get(Journal.title == 'fake journal entry'))
+
+    def test_create_tag(self):
+        """See of a tag was created in the setup """
+        self.assertTrue(Tag.get(Tag.tag == 'fake_tag'))
+
+    def tearDown(self):
+        User.get(User.username='fake_$_user').delete_instance()
+        Journal.get(Journal.title='fake journal entry').delete_instance()
+        Tag.get(Tag.tag='fake_tag').delete_instance()
+
+
